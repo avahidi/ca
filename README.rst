8d055e2a8aca403946619f73b8fe552dfe11f41c
Vahidi, Arash <arash.vahidi@gmail.com>
2025-01-19 23:36:12 +0100

    Corrected cache bug. Added history file. Improved README.

--- README.rst
+++ README.rst
@@ -1,1 +1,37 @@
-Cache system for short and repeating curl commands
+CA
+==
+
+CA is a local cache system for command-line information servers such as cheat.sh.
+
+It uses curl to download information per usual, but then caches the result for future access. This reduces server load and also makes pages accessible when offline.
+
+
+How to install
+--------------
+
+Build from source code::
+
+    go install github.com/avahidi/CA@latest
+
+
+How to use it
+-------------
+
+Simply replace "curl" with "ca"::
+
+    $ curl cht.sh/rust/pointers
+    Use references when you can, use pointers when you must. If you're not...
+
+    $ ca cht.sh/rust/pointers
+    Use references when you can, use pointers when you must. If you're not...
+
+Now the page is cached and next time you access it, it will be retrieved from local cache instead of network.
+
+
+More interestingly, you can create helper functions or aliases for different tasks. Here is an example for Golang (using bash)::
+
+    $ alias go?="ca --prefix https://cht.sh/go/"
+    $ go? strings
+    ...
+    $ go? :learn
+    ...
--- main.go
+++ main.go
@@ -1,48 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"encoding/base64"
-	"net/url"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
 	"os"
-	"path"
 	"os/exec"
+	"path"
 	"strings"
 	"time"
-	"io/ioutil"
 )
 
 const TIME_DATA_CACHE time.Duration = 14 * 24 * time.Hour
 const TIME_FAILURE_CACHE time.Duration = 2 * time.Hour
 
-var BASE_OPTIONS = []string { "-A", "Curl via CA v0.1"}
+var BASE_OPTIONS = []string{"--silent", "-A", "Curl via CA v0.1"}
+
+var cacheBase, historyFile, configFile string
 
 // config contains general app configuration
 type Config struct {
 	Curl string
-	CacheBase string
-	ConfigFile string
-	HistoryFile string
 }
 
-func NewConfig(cache_dir, history_file, config_file string) *Config {
-	return &Config {
-		Curl: "/usr/bin/curl",
-			CacheBase: cache_dir,
-			ConfigFile: config_file,
-			HistoryFile: history_file,
-		}
+func NewConfig() *Config {
+	return &Config{Curl: "/usr/bin/curl"}
 }
 
 func (c *Config) Load(filename string) error {
-	if filename != "" {
-		c.ConfigFile = filename
-	}
-
-	file, err := os.Open(c.ConfigFile)
+	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
@@ -53,11 +43,7 @@ func (c *Config) Load(filename string) error {
 }
 
 func (c Config) Save(filename string) error {
-	if filename != "" {
-		c.ConfigFile = filename
-	}
-
-	file, err := os.Create(c.ConfigFile)
+	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
@@ -67,10 +53,9 @@ func (c Config) Save(filename string) error {
 	return encoder.Encode(&c)
 }
 
-
 // Query is a helper class for the URL being accessed
 type Query struct {
-	Url *url.URL
+	Url    *url.URL
 	HostId string
 	PathId string
 }
@@ -80,7 +65,9 @@ func NewQuery(urlstr string) (*Query, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
 	host := url.Scheme + "//" + url.Host
 	if url.Port() != "" {
 		host += ":" + url.Port()
@@ -90,62 +77,64 @@ func NewQuery(urlstr string) (*Query, error) {
 		path = "/"
 	}
 
-	return &Query {
-		Url: url,
-			HostId: base64.URLEncoding.EncodeToString([]byte(host)),
-			PathId: base64.URLEncoding.EncodeToString([]byte(path)),
-		}, nil
+	return &Query{
+		Url:    url,
+		HostId: base64.URLEncoding.EncodeToString([]byte(host)),
+		PathId: base64.URLEncoding.EncodeToString([]byte(path)),
+	}, nil
 }
 
-func (q Query)CacheFile(c *Config) string {
-	return path.Join(c.CacheBase, q.HostId, q.PathId)
+func (q Query) CacheFile() string {
+	return path.Join(cacheBase, q.HostId, q.PathId)
 }
 
-func (q Query)CacheFolder(c *Config) string {
-	return path.Join(c.CacheBase, q.HostId)
+func (q Query) CacheFolder() string {
+	return path.Join(cacheBase, q.HostId)
 }
 
-func (q Query)CacheCheck(c *Config) (exists bool, isRecent bool)  {
-	filename := q.CacheFile(c)
+func (q Query) CacheCheck() (exists bool, isRecent bool) {
+	filename := q.CacheFile()
 	info, err := os.Stat(filename)
 	if err != nil {
 		return false, false
 	}
 
-	return true, time.Since(info.ModTime()) > TIME_DATA_CACHE
+	return true, time.Since(info.ModTime()) < TIME_DATA_CACHE
 }
 
-func (q Query)CacheRead(c *Config) ([]byte, error)  {
-	filename := q.CacheFile(c)
+func (q Query) CacheRead() ([]byte, error) {
+	filename := q.CacheFile()
 	return ioutil.ReadFile(filename)
 }
 
-func (q Query)CacheWrite(content []byte, c *Config) error {
-	folder := q.CacheFolder(c)
+func (q Query) CacheWrite(content []byte) error {
+	folder := q.CacheFolder()
 	if err := os.MkdirAll(folder, 0700); err != nil {
 		return err
 	}
 
-	filename := q.CacheFile(c)
+	filename := q.CacheFile()
 	return ioutil.WriteFile(filename, content, 0600)
 }
 
-
-type HistoryEntry struct {
-	Time  string
-	Query string
-}
-
-func Get(q *Query, c *Config, options []string) ([]byte, error) {
+func (q *Query) Get(c *Config, options []string) ([]byte, error) {
 	args := append(options, q.Url.String())
 	cmd := exec.Command(c.Curl, args...)
-	//return cmd.CombinedOutput()
-	return cmd.Output()
-
+	return cmd.Output() // cmd.CombinedOutput()
 }
 
+func recordHistory(q *Query, mode string) error {
+	f, err := os.OpenFile(historyFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
 
-func setup() (cache_dir, history_file, config_file string){
+	defer f.Close()
+	_, err = fmt.Fprintf(f, "%v - %s - %v\n", time.Now().Format(time.RFC3339), mode, q.Url)
+	return err
+}
+
+func setup() (cache_dir, history_file, config_file string) {
 	// set up the relevant folders and
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -153,7 +142,7 @@ func setup() (cache_dir, history_file, config_file string){
 	}
 
 	cache_dir = path.Join(home, ".cache/ca")
-	history_file  = path.Join(home, ".cache/ca/history")
+	history_file = path.Join(home, ".cache/ca/history")
 	config_file = path.Join(home, ".config/ca.conf")
 
 	if err := os.MkdirAll(cache_dir, 0700); err != nil {
@@ -163,8 +152,8 @@ func setup() (cache_dir, history_file, config_file string){
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr,"Usage:\n" +
-		"    %s [--prefix=<prefix>] [--o=<flag>] <query>\n" +
+	fmt.Fprintf(os.Stderr, "Usage:\n"+
+		"    %s [--prefix=<prefix>] [--o=<flag>] <query>\n"+
 		"    %s --purge",
 		os.Args[0], os.Args[0],
 	)
@@ -189,12 +178,12 @@ func parseArgs() (query string, options []string) {
 }
 
 func main() {
-	cache_dir, history_file, config_file := setup()
+	cacheBase, historyFile, configFile = setup()
 
 	// Load configuration
-	config := NewConfig(cache_dir, history_file, config_file)
-	config.Load("")
-	config.Save("")
+	config := NewConfig()
+	config.Load(configFile)
+	config.Save(configFile)
 
 	queryStr, options := parseArgs()
 	options = append(options, BASE_OPTIONS...)
@@ -205,19 +194,22 @@ func main() {
 	}
 
 	var content []byte
-	cexist, crecent := query.CacheCheck(config)
-
+	var mode string
+	cexist, crecent := query.CacheCheck()
 	if cexist && crecent {
-		content, err = query.CacheRead(config)
+		mode = "cached"
+		content, err = query.CacheRead()
 		if err != nil {
 			log.Fatalf("Failed to read content from cache: %v\n", err)
 		}
 	} else {
-		content, err = Get(query, config, options)
+		mode = "received"
+		content, err = query.Get(config, options)
 		if err != nil {
 			if cexist {
+				mode = "cache-backup"
 				log.Printf("Using old cache due to server failure: %v\n", err)
-				content, err = query.CacheRead(config)
+				content, err = query.CacheRead()
 				if err != nil {
 					log.Fatalf("Failed to read content from cache: %v\n", err)
 				}
@@ -225,12 +217,12 @@ func main() {
 				log.Fatalf("Failed to get content from server: %v\n", err)
 			}
 		} else {
-			err = query.CacheWrite(content, config)
+			err = query.CacheWrite(content)
 			if err != nil {
 				log.Printf("Unable to write to cache: %v\n", err)
 			}
 		}
 	}
-
+	recordHistory(query, mode)
 	fmt.Printf("%s", string(content))
 }
